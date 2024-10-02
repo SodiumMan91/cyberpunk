b03379dabaab3173f711744b7cc8c606c4fd8daa
SodiumMan91 <nb3236@columbia.edu>
2024-10-02 19:11:19 -0400

    Initial commit

---
+++ R/cyberpunk.R
@@ -0,0 +1,103 @@
+cyberpunk <- c(
+  "#E200F7",
+  "#0FF0FC",
+  "#450EFF",
+  "#D6014D",
+  "#02B59F",
+  "#ccccff",
+  "#ccff00",
+  "#FFFFFF"
+)
+
+#' @title The Cyberpunk palette
+#' @description The Cyberpunk palette
+#' @inheritDotParams ggplot2::discrete_scale
+#' @param n number of colors
+#' @param type discrete or continuous
+#' @param reverse reverse order, Default: FALSE
+#' @rdname cpunk
+#' @export
+#' @examples
+#' library(scales)
+#' show_col(cpunk_pal()(5))
+#' @importFrom scales manual_pal
+#' @importFrom grDevices colorRampPalette
+
+cpunk_pal <- function(n = NULL, type = c("discrete", "continuous"), reverse = FALSE) {
+  cpunk <- cyberpunk  # Your predefined color palette
+
+  if (reverse) {
+    cpunk <- rev(cpunk)
+  }
+
+  type <- match.arg(type)
+
+  if (type == "discrete") {
+    if (is.null(n)) {
+      n <- length(cpunk)
+    }
+
+    if (n > length(cpunk)) {
+      stop(paste0("Palette does not have ", n, " colors, maximum is ", length(cpunk), "!"))
+    }
+
+    return(scales::manual_pal(cpunk[1:n]))
+  } else {
+    return(grDevices::colorRampPalette(cpunk))
+  }
+}
+
+
+#' @title scale_color_cpunk
+#' @rdname cpunk_pal
+#' @export
+#' @examples
+#'
+#' library(ggplot2)
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_color_cpunk()
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+
+scale_color_cpunk <- function(n, type = "discrete",
+                                 reverse = FALSE, ...){
+  if (type == "discrete") {
+    ggplot2::discrete_scale("color", "cpunk", cpunk_pal(), ...)
+  } else {
+    ggplot2::scale_color_gradientn(colors = cpunk_pal(n = n, type = type,
+                                                         reverse = reverse)(8))
+  }
+}
+
+#' @title scale_colour_cpunk
+#' @rdname cpunk_pal
+#' @export
+#' @examples
+#'
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_color_cpunk()
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+
+scale_colour_cpunk <- scale_color_cpunk
+
+#' @title scale_fill_cpunk
+#' @rdname cpunk_pal
+#' @export
+#' @examples
+#'
+#' ggplot(mpg, aes(displ)) +
+#'      geom_histogram(aes(fill = class), col = "black", size = 0.1) +
+#'      scale_fill_cpunk()
+#' @importFrom ggplot2 discrete_scale scale_fill_gradientn
+
+scale_fill_cpunk <- function(n = NULL, type = "discrete", reverse = FALSE, ...) {
+  if (type == "discrete") {
+    ggplot2::discrete_scale("fill", "cpunk", cpunk_pal(type = "discrete", reverse = reverse), ...)
+  } else {
+    if (is.null(n)) n <- 256  # Default number of colors for continuous scale
+    ggplot2::scale_fill_gradientn(colors = cpunk_pal(type = type, reverse = reverse)(n), ...)
+  }
+}
---
+++ R/theme_cpunk.R
@@ -0,0 +1,102 @@
+
+#' @title The Cyberpunk Theme#'
+#'
+#' @param text.font text font, Default: mono
+#' @param title.font title font, Default: mono
+#' @param legend.font legend font, Default: mono
+#' @param title.size title font size, Default: 18
+#' @param text.size text font size, Default: 14
+#' @param subtitle.size subtitle font size, Default: 12
+#' @param axis.title.size axis title font size, Default: 14
+#' @param axis.text.size axis text font size, Default: 10
+#' @param legend.title.size legend title font size, Default: 10
+#' @param legend.text.size legend text font size, Default: 9
+#' @param title.color title color, Default = "#ffffff"
+#' @param subtitle.color subtitle color, Default = "#ffffff"
+#' @param text.color text color, Default = "#ffffff"
+#' @param axis.title.color axis title color, Default = "#ffffff"
+#' @param axis.text.color axis text color, Default = "#ffffff"
+#' @param legend.title.color legend title color, Default = "#ffffff"
+#' @param legend.text.color legend text color, Default = "#ffffff"
+#' @param legend.position legend position, Default: "bottom"
+#' @param ticks add axis ticks, Default: FALSE
+#' @examples
+#' ggplot(airquality, aes(x = Day, y = Temp, group = as.factor(Month),
+#'         color = as.factor(Month))) + geom_point(size = 2.5) +   theme_cpunk()
+#' @rdname theme_cpunk
+#' @seealso [ggplot2::theme]
+#' @importFrom ggplot2 theme element_text element_rect element_blank element_line
+#' theme_minimal
+#' @importFrom grid unit
+#' @return ggplot2 theme object
+#' @export
+
+
+theme_cpunk <- function(
+    text.font = "mono",
+    title.font = "mono",
+    legend.font = "mono",
+    title.size = 18,
+    text.size = 14,
+    subtitle.size = 12,
+    axis.title.size = 14,
+    axis.text.size = 10,
+    legend.title.size = 10,
+    legend.text.size = 9,
+    title.color = "#ffffff",
+    subtitle.color = "#ffffff",
+    text.color = "#ffffff",
+    axis.title.color = "#ffffff",
+    axis.text.color = "#ffffff",
+    legend.title.color = "#ffffff",
+    legend.text.color = "#ffffff",
+    legend.position = "bottom",
+    ticks = FALSE) {
+  cpunk <- ggplot2::theme_minimal() +
+      ggplot2::theme(
+        panel.background = element_rect(fill = "black", color = NA),
+        plot.background = element_rect(fill = "black"),
+        panel.grid.major = element_line(color = "gray30"),
+        panel.grid.minor = element_line(color = "gray20"),
+        plot.title = element_text(family = title.font, color = title.color, size = title.size, face = "bold"),
+        plot.subtitle = element_text(color = subtitle.color, size = subtitle.size),
+        axis.title = element_text(family = text.font, color = axis.title.color, size = axis.title.size, face = "bold"),
+        axis.text = element_text(family = text.font, color = axis.text.color, size = axis.text.size, face = "bold"),
+        legend.background = element_rect(fill = "black"),
+        legend.text = element_text(family = legend.font, color = legend.text.color, size = legend.text.size, face = "bold"),
+        legend.title = element_text(family = legend.font, color = legend.title.color, size = legend.title.size, face = "bold"),
+        legend.position = legend.position
+      )
+
+    scale_color_cpunk()
+    scale_fill_cpunk()
+    ggplot2::update_geom_defaults("point", list(color = "#E200F7", stroke = 1))
+    ggplot2::update_geom_defaults("line", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("bar", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("col", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("density", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("boxplot", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("violin", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("smooth", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("area", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("tile", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("text", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("errorbar", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("ribbon", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("polygon", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("path", list(color = "#E200F7"))
+    ggplot2::update_geom_defaults("step", list(color = "#E200F7"))
+
+    if (ticks == FALSE) {
+      cpunk <- cpunk + theme(axis.ticks = element_blank(),
+                             axis.ticks.x = element_blank(),
+                             axis.ticks.y = element_blank())
+    } else {
+      cpunk <- cpunk + theme(axis.ticks = element_line(linewidth = 0.15, color = "white"),
+                             axis.ticks.x = element_line(linewidth = 0.15, color = "white"),
+                             axis.ticks.y = element_line(linewidth = 0.15, color = "white"),
+                             axis.ticks.length = grid::unit(4, "pt"))
+    }
+
+    return(cpunk)
+}
