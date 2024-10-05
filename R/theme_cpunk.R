5c7ca6ecf8b26e55fbcab87781a05c7052bff322
SodiumMan91 <nb3236@columbia.edu>
2024-10-05 15:08:29 -0400

    updated strip.text

--- R/theme_cpunk.R
+++ R/theme_cpunk.R
@@ -56,8 +56,10 @@ theme_cpunk <- function(
       ggplot2::theme(
         panel.background = element_rect(fill = "black", color = NA),
         plot.background = element_rect(fill = "black"),
-        panel.grid.major = element_line(color = "gray"),
-        panel.grid.minor = element_line(color = "gray"),
+        panel.grid.major = element_line(color = "gray30"),
+        panel.grid.minor = element_line(color = "gray30"),
+        strip.text = elemnt_text(family = title.font, color = title.color, size = title.size, face = "bold"),
+        strip.background = element_rect(fill = "grey20", color = "black")
         plot.title = element_text(family = title.font, color = title.color, size = title.size, face = "bold"),
         plot.subtitle = element_text(color = subtitle.color, size = subtitle.size),
         axis.title = element_text(family = text.font, color = axis.title.color, size = axis.title.size, face = "bold"),
